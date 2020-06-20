e559d048597e15ff49abd3aa301bb76adad002cb
Arpit Kaushal <kaushal.arpit@gmail.com>
2020-06-21 03:28:12 +0530

    Task 2

--- cmdhist.md
+++ cmdhist.md
@@ -1,8 +1,11 @@
 ># Table of Contents
 1. [Setting up ChampSim on your System](#setting-up-champsim-on-your-system)
 2. [Task 1](#task-1)
+3. [Task 2](#task-2)
 
 ## Setting up ChampSim on your System
+I used WSL Ubuntu, and had to run the following commands to have finally been able to run ChampSim. See if this helps you!
+
 ``` bash
 sudo apt-get install build-essential
 sudo apt-get install make
@@ -28,29 +31,11 @@ So, run the default test command now.
 
 ## Task 1
 1. Go to the root directory of ChampSim, and `cd scripts`
-2. Make a new file `task-1.sh` by running the command - `touch task-1.sh`.
-3. Open the file, and copy-paste the below code snippet. Save the file. 
-   [`Alternative`] Download `task-1.sh` from [here](scripts/task-1.sh) and paste it the `scripts` folder.     
+2. Download `task-1.sh` from [here](scripts/task-1.sh) and paste it in the `scripts` folder.     
+3. Now, make sure that you're in the `scripts` directory, and then, run, 
+   `time sh ./task-1.sh`
 
-``` bash
-cd ..
-
-./build_champsim.sh bimodal no no no no lru 1   2>/dev/null # single core build
-echo '\n\nBuild Completed. Running the 1 core build.\n\n' 
-./run_champsim.sh bimodal-no-no-no-no-lru-1core 1 10 400.perlbench-41B.champsimtrace.xz # single core run
-echo '\n\nTask 1 Part 1 completed!\n\n'
-
-
-./build_champsim.sh bimodal no no no no lru 4   2>/dev/null # four core build
-echo '\n\nBuild Completed. Running the 4 core build.\n\n' 
-./run_4core.sh bimodal-no-no-no-no-lru-4core 1 10 0 400.perlbench-41B.champsimtrace.xz 454.calculix-104B.champsimtrace.xz 603.bwaves_s-5359B.champsimtrace.xz 649.fotonik3d_s-1B.champsimtrace.xz # 4 traces run
-echo '\n\nTask 1 Part 2 completed!\n\n'
-
-echo '\n\nTask 1 completed! :)\n\n'
-
-exit
-```
-
-4. Now, make sure that you're in the `scripts` directory, and then, run, 
-   `sh ./task-1.sh`
+## Task 2
+1. Download `task-2.sh` from [here](scripts/task-2.sh) and paste it in the `scripts` folder.     
+2. Now, make sure that you're in the `scripts` directory, and then, run, 
  
---
+++ scripts/task-2.sh
@@ -0,0 +1,95 @@
+# !/bin/bash
+cd ..
+
+if [ ! -d "../logs" ] 
+then
+    mkdir logs  
+fi
+
+echo  'TASK 2\nStart Time: ' >> ./logs/task-2.txt
+echo  'TASK 2 ERRORS/WARNINGS\n' >> ./logs/task-2-err.txt
+
+exec 1>> ./logs/task-2.txt 
+exec 2>> ./logs/task-2-err.txt 
+
+SECONDS=0 # reset BASH time counter
+echo 'Script started at $SECONDS'
+
+# NOTE: You only need to build once for a configuration. 
+# BUILDING 
+./build_champsim.sh bimodal no no no no lru 1           
+echo '\n\nBuild bimodal completed. \n\n'                
+./build_champsim.sh gshare no no no no lru 1            
+echo '\n\nBuild gshare completed. \n\n'                 
+./build_champsim.sh hashed_perceptron no no no no lru 1 
+echo '\n\nBuild  hashed_perceptron completed. \n\n'     
+./build_champsim.sh perceptron no no no no lru 1        
+echo '\n\nBuild perceptron completed. \n\n'             
+
+echo 'The BUILDS happend at $SECONDS'
+
+# RUNNING BUILDS
+
+# BIMODAL
+    ./run_champsim.sh bimodal-no-no-no-no-lru-1core 1 10 454.calculix-104B.champsimtrace.xz      
+    echo 'Bimodal 454 done! :)'  
+    ./run_champsim.sh bimodal-no-no-no-no-lru-1core 1 10 603.bwaves_s-5359B.champsimtrace.xz      
+    echo 'Bimodal 603 done! :)'  
+    ./run_champsim.sh bimodal-no-no-no-no-lru-1core 1 10 649.fotonik3d_s-1B.champsimtrace.xz     
+    echo 'Bimodal 649 done! :)'  
+    ./run_champsim.sh bimodal-no-no-no-no-lru-1core 1 10 654.roms_s-1021B.champsimtrace.xz        
+    echo 'Bimodal 654 done! :)'  
+
+    echo '\n\nTask 2 Part bimodal completed! Time Elapsed: $SECONDS\n\n'
+
+# GSHARE
+    ./run_champsim.sh gshare-no-no-no-no-lru-1core 1 10 454.calculix-104B.champsimtrace.xz      
+    echo 'gshare 454 done! :)'  
+    ./run_champsim.sh gshare-no-no-no-no-lru-1core 1 10 603.bwaves_s-5359B.champsimtrace.xz      
+    echo 'gshare 603 done! :)'  
+    ./run_champsim.sh gshare-no-no-no-no-lru-1core 1 10 649.fotonik3d_s-1B.champsimtrace.xz     
+    echo 'gshare 649 done! :)'  
+    ./run_champsim.sh gshare-no-no-no-no-lru-1core 1 10 654.roms_s-1021B.champsimtrace.xz        
+    echo 'gshare 654 done! :)'  
+
+    echo '\n\nTask 2 Part gshare completed! Time Elapsed: $SECONDS \n\n'
+
+
+# HASHED_PERCEPTRON
+    ./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 1 10 454.calculix-104B.champsimtrace.xz      
+    echo 'hashed_perceptron 454 done! :)'  
+    ./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 1 10 603.bwaves_s-5359B.champsimtrace.xz      
+    echo 'hashed_perceptron 603 done! :)'  
+    ./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 1 10 649.fotonik3d_s-1B.champsimtrace.xz     
+    echo 'hashed_perceptron 649 done! :)'  
+    ./run_champsim.sh hashed_perceptron-no-no-no-no-lru-1core 1 10 654.roms_s-1021B.champsimtrace.xz        
+    echo 'hashed_perceptron 654 done! :)'  
+
+    echo '\n\nTask 2 Part hashed_perceptron completed! Time Elapsed: $SECONDS \n\n'
+
+
+# PERCEPTRON
+    ./run_champsim.sh perceptron-no-no-no-no-lru-1core 1 10 454.calculix-104B.champsimtrace.xz      
+    echo 'perceptron 454 done! :)'  
+    ./run_champsim.sh perceptron-no-no-no-no-lru-1core 1 10 603.bwaves_s-5359B.champsimtrace.xz      
+    echo 'perceptron 603 done! :)'  
+    ./run_champsim.sh perceptron-no-no-no-no-lru-1core 1 10 649.fotonik3d_s-1B.champsimtrace.xz     
+    echo 'perceptron 649 done! :)'  
+    ./run_champsim.sh perceptron-no-no-no-no-lru-1core 1 10 654.roms_s-1021B.champsimtrace.xz        
+    echo 'perceptron 654 done! :)'  
+
+    echo '\n\nTask 2 Part perceptron completed! Time Elapsed: $SECONDS \n\n'
+
+
+echo 'TASK 2 COMPLETED! :)\n Time Elapsed: $SECONDS \n\n\n\n\n'
+
+exit
+
+# run without build took 7m 41s
+
+# Traces
+# 400.perlbench-41B.champsimtrace.xz
+# 454.calculix-104B.champsimtrace.xz
+# 603.bwaves_s-5359B.champsimtrace.xz
+# 649.fotonik3d_s-1B.champsimtrace.xz
+# 654.roms_s-1021B.champsimtrace.xz
